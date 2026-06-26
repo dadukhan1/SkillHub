96a55b9d08493b5e39057d3bd041a37884e71367
Dadu Khan <photoshopt65@gmail.com>
2026-06-26 17:59:56 +0500

    feat(config, layout): update image domains and enhance layout structure
    - Added new image domains for Google and GitHub avatars in next.config.ts.
    - Improved layout structure by adjusting body attributes in layout.tsx for better hydration handling.

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -33,7 +33,10 @@ export default function RootLayout({
       suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full bg-background text-foreground">
+      <body
+        suppressHydrationWarning
+        className="min-h-full bg-background text-foreground"
+      >
         <NextAuthProvider>
           <StoreProvider>
             <ThemeProvider>{children}</ThemeProvider>
